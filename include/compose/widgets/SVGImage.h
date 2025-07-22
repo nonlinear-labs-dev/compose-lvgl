6120102937ece5edeff968847008915d37f2ce19
schlenkibus <justus.scheil@gmx.de>
2025-07-22 13:32:56 +0200

    implement svg rendering and transform to desired size

--- include/compose/widgets/SVGImage.h
+++ include/compose/widgets/SVGImage.h
@@ -20,6 +20,8 @@ namespace Compose
     void setModifier(const SVGPath& p) const;
     void setModifier(PixelSize s) const;
     void setModifier(PrimaryColor col) const override;
+    
+    Size getViewboxSize() const;
   };
 }
 
--- src/widgets/SVGImage.cpp
+++ src/widgets/SVGImage.cpp
@@ -6,6 +6,18 @@ namespace Compose
   void SVGImage::setModifier(const SVGPath &p) const
   {
     lv_image_set_src(getHandle(), std::format("S:{}", p.it.c_str()).c_str());
+
+    const auto viewboxSize = getViewboxSize();
+    if(p.size.has_value())
+    {
+      const auto [w, h] = p.size.value();
+      lv_obj_set_size(getHandle(), p.size.value().w, p.size.value().h);
+      constexpr static auto baseZoom = 256;
+      const auto scaleX = w / static_cast<float>(viewboxSize.w);
+      const auto scaleY = h / static_cast<float>(viewboxSize.h);
+      lv_image_set_scale_x(getHandle(), scaleX * baseZoom);
+      lv_image_set_scale_y(getHandle(), scaleY * baseZoom);
+    }
   }
 
   void SVGImage::setModifier(PrimaryColor c) const
@@ -16,5 +28,11 @@ namespace Compose
                                    .green = c.g,
                                    .red = c.r,
                                });
+  }
+
+  Size SVGImage::getViewboxSize() const
+  {
+    return Size { static_cast<int>(lv_image_get_src_width(getHandle())),
+                  static_cast<int>(lv_image_get_src_height(getHandle())) };
   }
 }
