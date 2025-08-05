94058095b88da57cd407bf9347a0728ecf8473a2
schlenkibus <justus.scheil@gmx.de>
2025-08-05 23:10:08 +0200

    WIP refactor

--- src/widgets/CustomDrawingElement.cpp
+++ src/widgets/CustomDrawingElement.cpp
@@ -6,128 +6,107 @@
 
 namespace Compose
 {
-  struct CanvasBufferData
+  struct CanvasData
   {
-    std::shared_ptr<lv_draw_buf_t> buffer;
+    lv_draw_buf_t* buffer = nullptr;
     CustomDrawingElement::tDrawCB drawCallback;
+    lv_event_dsc_t* resizeHandler = nullptr;
+    lv_event_dsc_t* readyHandler = nullptr;
+    lv_event_dsc_t* deleteHandler = nullptr;
 
-    explicit CanvasBufferData(CustomDrawingElement::tDrawCB cb)
-        : buffer(nullptr)
-        , drawCallback(std::move(cb))
+    CanvasData(lv_obj_t* handle, CustomDrawingElement::tDrawCB cb)
+        : drawCallback(std::move(cb))
     {
-    }
-  };
-
-  struct SharedPtrWrapper
-  {
-    std::shared_ptr<CanvasBufferData> data_ptr;
-
-    explicit SharedPtrWrapper(std::shared_ptr<CanvasBufferData> data)
-        : data_ptr(std::move(data))
-    {
-    }
-  };
-
-  constexpr static auto CANVAS_DATA_KEY = "_CanvasDataKey";
-
-  auto createNewBuffer(int w, int h)
-  {
-    auto rawBuffer = lv_draw_buf_create(w, h, LV_COLOR_FORMAT_ARGB8888, LV_STRIDE_AUTO);
-    return std::shared_ptr<lv_draw_buf_t>(rawBuffer,
-                                          [](lv_draw_buf_t* buf)
-                                          {
-                                            if(buf)
-                                              lv_draw_buf_destroy(buf);
-                                          });
-  }
-
-  static void handleResizeEvent(lv_event_t* e)
-  {
-    if(lv_event_get_code(e) == LV_EVENT_SIZE_CHANGED)
-    {
-      const auto wrapper = static_cast<SharedPtrWrapper*>(lv_event_get_user_data(e));
-      if(wrapper && wrapper->data_ptr)
-      {
-        const auto handle = static_cast<lv_obj_t*>(lv_event_get_target(e));
-        const int width = lv_obj_get_width(handle);
-        const int height = lv_obj_get_height(handle);
-
-        if(width > 0 && height > 0)
-        {
-          const auto newBuffer = createNewBuffer(width, height);
-
-          if(newBuffer != nullptr)
+      resizeHandler = lv_obj_add_event_cb(
+          handle,
+          [](lv_event_t* e)
           {
-            wrapper->data_ptr->buffer = newBuffer;
-            lv_canvas_set_draw_buf(handle, newBuffer.get());
-            LVGLDrawContext drawContext(handle);
-            wrapper->data_ptr->drawCallback(drawContext, width, height);
-          }
-        }
-      }
+            if(lv_event_get_code(e) == LV_EVENT_SIZE_CHANGED)
+            {
+              const auto data = static_cast<CanvasData*>(lv_event_get_user_data(e));
+              if(data)
+              {
+                const auto handle = static_cast<lv_obj_t*>(lv_event_get_target(e));
+                const int width = lv_obj_get_width(handle);
+                const int height = lv_obj_get_height(handle);
+
+                if(width > 0 && height > 0)
+                {
+                  if(data->buffer)
+                    lv_draw_buf_destroy(data->buffer);
+
+                  data->buffer = lv_draw_buf_create(width, height, LV_COLOR_FORMAT_ARGB8888, LV_STRIDE_AUTO);
+                  if(data->buffer)
+                  {
+                    lv_canvas_set_draw_buf(handle, data->buffer);
+                    LVGLDrawContext drawContext(handle);
+                    data->drawCallback(drawContext, width, height);
+                  }
+                }
+              }
+            }
+          },
+          LV_EVENT_SIZE_CHANGED, this);
+
+      readyHandler = lv_obj_add_event_cb(
+          handle,
+          [](lv_event_t* e)
+          {
+            const auto handle = static_cast<lv_obj_t*>(lv_event_get_target(e));
+            const auto data = static_cast<CanvasData*>(lv_event_get_user_data(e));
+            lv_obj_update_layout(handle);
+            if(data && data->drawCallback)
+            {
+              LVGLDrawContext drawContext(handle);
+              data->drawCallback(drawContext, lv_obj_get_width(handle), lv_obj_get_height(handle));
+            }
+          },
+          LV_EVENT_READY, this);
+
+      deleteHandler = lv_obj_add_event_cb(
+          handle,
+          [](lv_event_t* e)
+          {
+            if(lv_event_get_code(e) == LV_EVENT_DELETE)
+            {
+              const auto data = static_cast<CanvasData*>(lv_event_get_user_data(e));
+              if(data)
+              {
+                if(data->resizeHandler)
+                  lv_obj_remove_event_dsc(static_cast<lv_obj_t*>(lv_event_get_target(e)), data->resizeHandler);
+                if(data->readyHandler)
+                  lv_obj_remove_event_dsc(static_cast<lv_obj_t*>(lv_event_get_target(e)), data->readyHandler);
+                if(data->deleteHandler)
+                  lv_obj_remove_event_dsc(static_cast<lv_obj_t*>(lv_event_get_target(e)), data->deleteHandler);
+                if(data->buffer)
+                  lv_draw_buf_destroy(data->buffer);
+              }
+            }
+          },
+          LV_EVENT_DELETE, this);
     }
-  }
+
+    ~CanvasData()
+    {
+    }
+  };
+
+  static constexpr auto CANVAS_DATA_KEY = "CanvasData";
 
   void CustomDrawingElement::setDrawCall(tDrawCB&& draw) const
   {
-    auto handle = getHandle();
-    if(!handle)
-      return;
-
-    auto bufferData = std::make_shared<CanvasBufferData>(std::move(draw));
-    auto* wrapper = new SharedPtrWrapper(bufferData);
-
-    ensureDataForKeyExistsNonOwning<SharedPtrWrapper>(CANVAS_DATA_KEY, [wrapper] { return wrapper; });
-
-    lv_obj_add_event_cb(handle, &handleResizeEvent, LV_EVENT_SIZE_CHANGED, wrapper);
-    lv_obj_add_event_cb(
-        handle,
-        [](lv_event_t* e)
+    doAutorun(
+        [handle = getHandle(),
+         canvasData = &ensureDataForKeyExistsOwning<CanvasData>(
+             CANVAS_DATA_KEY, [this, &draw] { return new CanvasData(getHandle(), std::move(draw)); })]
         {
-          if(lv_event_get_code(e) == LV_EVENT_DELETE)
-          {
-            delete static_cast<SharedPtrWrapper*>(lv_event_get_user_data(e));
-          }
-        },
-        LV_EVENT_DELETE, wrapper);
-
-    lv_obj_update_layout(handle);
-    const auto width = lv_obj_get_width(handle);
-    const auto height = lv_obj_get_height(handle);
-
-    if(width <= 0 || height <= 0)
-      return;
-
-    const auto newBuffer = createNewBuffer(width, height);
-
-    if(newBuffer == nullptr)
-      return;
-
-    bufferData->buffer = newBuffer;
-    lv_canvas_set_draw_buf(handle, newBuffer.get());
-
-    lv_obj_add_event_cb(
-        handle,
-        [](lv_event_t* e)
-        {
-          const auto handle = static_cast<lv_obj_t*>(lv_event_get_target(e));
-          const auto wrapper = static_cast<SharedPtrWrapper*>(lv_event_get_user_data(e));
-          lv_obj_update_layout(handle);
-          if(wrapper && wrapper->data_ptr)
+          if(handle && canvasData && canvasData->drawCallback)
           {
             LVGLDrawContext drawContext(handle);
-            wrapper->data_ptr->drawCallback(drawContext, lv_obj_get_width(handle), lv_obj_get_height(handle));
+            const auto w = lv_obj_get_width(handle);
+            const auto h = lv_obj_get_height(handle);
+            canvasData->drawCallback(drawContext, w, h);
           }
-        },
-        LV_EVENT_READY, wrapper);
-
-    doAutorun(
-        [handle = getHandle(), wrapper]
-        {
-          LVGLDrawContext drawContext(handle);
-          const auto w = lv_obj_get_width(handle);
-          const auto h = lv_obj_get_height(handle);
-          wrapper->data_ptr->drawCallback(drawContext, w, h);
         });
   }
 }
