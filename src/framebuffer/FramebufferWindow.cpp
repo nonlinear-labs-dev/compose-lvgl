a7f75b58fea7ffaa20319c484f4c977491f0f5f3
schlenkibus <justus.scheil@gmx.de>
2026-01-13 18:56:16 +0100

    make touch work

--- src/framebuffer/FramebufferWindow.cpp
+++ src/framebuffer/FramebufferWindow.cpp
@@ -10,7 +10,7 @@ namespace Compose
     lv_disp_t *disp = lv_linux_fbdev_create();
     lv_linux_fbdev_set_file(disp, "/dev/fb0");
 
-    lv_indev_t *indev = lv_evdev_create(LV_INDEV_TYPE_POINTER, "/dev/input/event0");
+    lv_indev_t *indev = lv_evdev_create(LV_INDEV_TYPE_POINTER, "/dev/input/event5");
     lv_indev_set_display(indev, disp);
 
     lv_display_set_offset(disp, position.pos.x, position.pos.y);
