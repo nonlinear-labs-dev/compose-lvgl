6e89e4cdf7dd5f71bddf94a0c77c3fa6cdaa30fe
Matthias Seeber <matthias.seeber@nonlinear-labs.de>
2026-04-26 22:40:17 +0200

    introduced helper for drawing signal connections, added connections

--- include/compose/modifiers/Rect.h
+++ include/compose/modifiers/Rect.h
@@ -10,6 +10,7 @@ namespace Compose
     [[nodiscard]] Point moveByX(int i) const;
     [[nodiscard]] Point moveByY(int i) const;
     [[nodiscard]] Point moveByXY(int ix, int iy) const;
+    [[nodiscard]] Point midPoint(const Point p) const;
   };
 
   inline Point Point::moveByX(int i) const
@@ -27,6 +28,12 @@ namespace Compose
     return { x + ix, y + iy };
   }
 
+  inline Point Point::midPoint(const Point p) const
+  {
+    const int dx = p.x - x, dy = p.y - y;
+    return moveByXY(dx >> 1, dy >> 1);
+  }
+
   struct PointF
   {
     float x = {};
@@ -39,10 +46,14 @@ namespace Compose
     Size size {};
     [[nodiscard]] Rect biggerBy(int i) const;
     [[nodiscard]] Rect expanded(int horizontal, int vertical) const;
-    [[nodiscard]] Point nw() const;
-    [[nodiscard]] Point ne() const;
-    [[nodiscard]] Point sw() const;
-    [[nodiscard]] Point se() const;
+    [[nodiscard]] Point northWest() const;
+    [[nodiscard]] Point north() const;
+    [[nodiscard]] Point northEast() const;
+    [[nodiscard]] Point east() const;
+    [[nodiscard]] Point southEast() const;
+    [[nodiscard]] Point south() const;
+    [[nodiscard]] Point southWest() const;
+    [[nodiscard]] Point west() const;
   };
 
   inline Rect Rect::biggerBy(int i) const
@@ -59,23 +70,43 @@ namespace Compose
     return ret;
   }
 
-  inline Point Rect::nw() const
+  inline Point Rect::northWest() const
   {
     return pos;
   }
 
-  inline Point Rect::ne() const
+  inline Point Rect::north() const
+  {
+    return pos.moveByX(size.w >> 1);
+  }
+
+  inline Point Rect::northEast() const
   {
     return pos.moveByX(size.w);
   }
 
-  inline Point Rect::sw() const
+  inline Point Rect::east() const
+  {
+    return pos.moveByXY(size.w, size.h >> 1);
+  }
+
+  inline Point Rect::southEast() const
+  {
+    return pos.moveByXY(size.w, size.h);
+  }
+
+  inline Point Rect::south() const
+  {
+    return pos.moveByXY(size.w >> 1, size.h);
+  }
+
+  inline Point Rect::southWest() const
   {
     return pos.moveByY(size.h);
   }
 
-  inline Point Rect::se() const
+  inline Point Rect::west() const
   {
-    return pos.moveByXY(size.w, size.h);
+    return pos.moveByY(size.h >> 1);
   }
 }
